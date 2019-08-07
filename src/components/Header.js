9d76023556787a33b31fbd3fcb21b734c9b3ddfa
nczop <naati.czop@gmail.com>
2019-08-07 11:21:44 +0200

    socialmedia

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,16 @@ export class Header extends Component {
     render() {
         return (
             <div className = "header"> 
-                <i className="fa fa-search" aria-hidden="true" id="search"></i>   
-                <i className="fa fa-shopping-cart" aria-hidden="true" id="shoppingBasket"></i>         
+                <div className = "socialmedia">
+                    <i className="fa fa-facebook-official" aria-hidden="true" id="facebook"></i>
+                    <i class="fa fa-instagram" aria-hidden="true" id="instagram"></i>
+                </div>
+                <div className = "shopingCart">
+                    <i className="fa fa-search" aria-hidden="true" id="search"></i>   
+                    <i className="fa fa-shopping-cart" aria-hidden="true" id="shoppingBasket"></i>
+                </div>
+                
+                         
             </div>
         )
     }
