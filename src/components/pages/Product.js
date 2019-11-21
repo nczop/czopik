4f0b5ede51485701f447eee574c218c59d82650f
nczop <naati.czop@gmail.com>
2019-11-21 18:23:00 +0100

    chomik nic nie nabroil

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -8,32 +8,29 @@ class Product extends Component {
     state = {
         product: {}
     }
-    
+
     componentDidMount() {
-        axios.get(`http://localhost:5000/shop/`+ this.props.match.params.id)
-          .then(res => {
-            this.setState({ product: res.data });
-          })
-        }  
-    
+        axios.get(`http://localhost:5000/shop/` + this.props.match.params.id)
+            .then(res => {
+                this.setState({ product: res.data });
+            })
+    }
+
     render() {
         console.log(this.state)
-        const {product} = this.state;
+        const { product } = this.state;
         return (
-            <div className= "singleContainer">
-                <div className= "singleProduct">
+            <div className="singleContainer">
+                <div className="singleProduct">
                     <div className="photoProduct"><img src={product.photo}></img></div>
                     <div className="titleDescriptionPrice">
                         <div className="titleProduct"> {product.title}</div>
                         <div className="descriptionProduct">Opis</div>
                         <div className="price">{product.price}</div>
-                    </div>                    
+                    </div>
                 </div>
-            )
-        } 
-        return <h1>nie dziala</h1>
-        
+            </div>
+        )
     }
 }
-
 export default withRouter(Product)
